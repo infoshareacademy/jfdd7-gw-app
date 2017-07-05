59359d4ede2d7e4bb8944a8095e1b6bc4fd9c122
Mateusz Tarsiński <tars.mat@gmail.com>
2017-07-05 17:01:13 +0200

    dodanie MainMenu, Categories i First Page

---
+++ src/Categories.js
@@ -0,0 +1,20 @@
+/**
+ * Created by mateusztarsinski on 05.07.17.
+ */
+import React from 'react'
+
+import {} from 'react-bootstrap'
+
+export default class Categories extends React.Component {
+
+  render() {
+    return (
+      <div className="Categories">
+        <h1>KATEGORIE</h1>
+        <h2>Kategoria</h2>
+        <h2>Kategoria</h2>
+        <h2>Kategoria</h2>
+      </div>
+    )
+  }
+}
---
+++ src/FirstPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {} from 'react-bootstrap'
+
+export default class FirstPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
