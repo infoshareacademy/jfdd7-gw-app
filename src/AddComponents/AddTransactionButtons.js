165f94ee09ba1dd0d6c1d4a6f6580061757c7639
piotrmichalek89 <piotrmichalek89@gmail.com>
2017-07-06 21:41:38 +0200

    Buttons below the form

--- src/AddComponents/AddTransactionButtons.js
+++ src/AddComponents/AddTransactionButtons.js
@@ -1,23 +1,19 @@
 import React from 'react'
-import {ButtonToolbar, Button} from 'react-bootstrap'
+import {Button} from 'react-bootstrap'
+import './AddTransactionButtons.css'
 
 export default class AddTransactionButtons extends React.Component {
 
   render() {
-    return(
+    return (
 
-      <div>
-        <ButtonToolbar>
-          <Button bsStyle="primary" bsSize="large">Large button</Button>
-          <Button bsSize="large">Large button</Button>
-        </ButtonToolbar>
-
-        <ButtonToolbar>
-          <Button bsStyle="primary" bsSize="large">Large button</Button>
-          <Button bsSize="large">Large button</Button>
-        </ButtonToolbar>
-
-      </div>
+<div className='container'>
+        <div className="row">
+        {/*<i class="fa fa-plus-square" aria-hidden="true"></i>*/}
+        <Button className='left col-xs-12 col-md-5' bsStyle="success">Zapisz i dodaj następny wpis</Button>
+        <Button className='right col-xs-12 col-md-5' bsStyle="success">Zapisz i opuść formularz</Button>
+        </div>
+</div>
 
     )
 
