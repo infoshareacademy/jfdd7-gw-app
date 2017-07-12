c7666870d173ee8b0be3519e2bc3be839f926b91
Mateusz Tarsiński <tars.mat@gmail.com>
2017-07-12 16:08:32 +0200

    table with history of transaction

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -20,49 +20,64 @@ export default connect(
       this.props.fetchTransactions()
     }
 
-// class Histories extends React.Component {
-//
-//   state = {
-//     transactions: []
-//   }
-//
-//   componentWillMount() {
-//     fetch(
-//       process.env.PUBLIC_URL + '/data/transactions.json'
-//     ).then(
-//       response => response.json()
-//     ).then(
-//       transactions => this.setState({
-//         transactions
-//       })
-//     ).catch(
-//       error => console.log(error.message)
-//     )
-//   }
 
     render() {
       const {data, fetching, error} = this.props.transactions
       return (
         <div>
-          <h1>Transactions</h1>
-          <ul>
-
-            { data !== null && data.map(
-              transaction => (
-                <li key={transaction.id}>
-                  {
-                    Object.keys(transaction).map(
-                      key => <span>{transaction[key]}</span>
-                    )
-                  }
-                  {/*{transaction.date}, {transaction.value}, {transaction.category}*/}
-                </li>
+          <Table bordered striped hover responsive>
+            <thead>
+            <tr>
+              <th>numer</th>
+              <th>data</th>
+              <th>wartość</th>
+              <th>kategoria</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+              //this.props.students.data !== null && this.props.students.data.map(
+              data !== null && data.map(
+                transaction => (
+                  <tr key={transaction.id}>
+                    <td>
+                      { transaction.id }
+                    </td>
+                    <td>
+                      { transaction.date }
+                    </td>
+                    <td>
+                      { transaction.value }
+                    </td>
+                    <td>
+                      { transaction.category }
+                    </td>
+                  </tr>
+                )
               )
-            )
             }
-          </ul>
+            </tbody>
+          </Table>
         </div>
       )
     }
   }
 )
+
+// <h1>Transactions</h1>
+// <ul>
+//
+// { data !== null && data.map(
+//   transaction => (
+//     <li key={transaction.id}>
+//       {
+//         Object.keys(transaction).map(
+//           key => <span>{transaction[key]}</span>
+//         )
+//       }
+//       {/*{transaction.date}, {transaction.value}, {transaction.category}*/}
+//     </li>
+//   )
+// )
+// }
+// </ul>
