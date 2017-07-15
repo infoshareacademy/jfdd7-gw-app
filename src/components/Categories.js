d14eba5c1aaae7737901c7dbcdd2682cf774d043
ProgramistaiJa <programista@i-ja.pl>
2017-07-15 13:12:32 +0200

    Show/Hide category transactions - don't work

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -9,75 +9,78 @@ import {activateFilter} from '../state/valuesFilters'
 
 
 export default connect(
-  state => ({
-    transactions: state.transactions,
-      activeFilterNames: state.valuesFilters.activeFilterNames
-  }),
-  dispatch => ({
-      fetchTransactions: () => dispatch(fetchTransactions()),
-      activateFilter: (name) => dispatch(activateFilter(name)),
-  })
+    state => ({
+        transactions: state.transactions,
+        activeFilterNames: state.valuesFilters.activeFilterNames
+    }),
+    dispatch => ({
+        fetchTransactions: () => dispatch(fetchTransactions()),
+        activateFilter: (name) => dispatch(activateFilter(name)),
+    })
 )(
-  class Categories extends React.Component {
+    class Categories extends React.Component {
 
-    componentWillMount() {
-      this.props.fetchTransactions()
-    }
+        componentWillMount() {
+            this.props.fetchTransactions()
+        }
 
-    render() {
-      const {data, fetching, error} = this.props.transactions
+        render() {
+            const {data, fetching, error} = this.props.transactions
 
-      return (
-        <div className="Categories">
-          <h1>KATEGORIE</h1>
+            return (
+                <div className="Categories">
+                    <h1>KATEGORIE</h1>
 
-          { error === null ? null : <p>{error.message}</p> }
-          { fetching === false ? null : <p>Fetching data...</p>}
+                    { error === null ? null : <p>{error.message}</p> }
+                    { fetching === false ? null : <p>Fetching data...</p>}
 
 
-          {
-            //this.props.students.data !== null && this.props.students.data.map(
-            data !== null && data.map(
-              transaction => transaction.category
-            ).filter(
-              (item, index, allItems) => allItems.indexOf(item) === index
-            ).map(
-              category => {
-                const dataToDisplay = data.filter(transaction => transaction.category === category)
-                  const buttons = [
-                      {
-                          label: category,
-                          filterName: category+'_visible'
-                      }
-                  ]
+                    {
+                        //this.props.students.data !== null && this.props.students.data.map(
+                        data !== null && data.map(
+                            transaction => transaction.category
+                        ).filter(
+                            (item, index, allItems) => allItems.indexOf(item) === index
+                        ).map(
+                            category => {
+                                const dataToDisplay = data.filter(transaction => transaction.category === category)
+                                const buttons = [
+                                    {
+                                        label: category,
+                                        filterName: category + '_visible'
+                                    }
+                                ]
 
-                  return (
-                  <div key={category}>
-                      {
-                          buttons.map(
-                              button => (
-                                  <Button
-                                      key={button.filterName}
-                                      onClick={() => this.props.activateFilter(button.filterName)}
-                                      //active={this.props.activeFilterNames.includes(button.filterName)}
-                                  >
-                                      {button.label}
-                                  </Button>
-                              )
-                          )
-                      }
-                    <CategoryTransactions
-                      transactions={dataToDisplay}
-                    />
-                  </div>
-                )
-              }
+                                return (
+                                    <div key={category}>
+                                        {
+                                            buttons.map(
+                                                button => (
+                                                    <Button
+                                                        key={button.filterName}
+                                                        onClick={() => this.props.activateFilter(button.filterName)}
+                                                        //active={this.props.activeFilterNames.includes(button.filterName)}
+                                                    >
+                                                        {button.label}
+                                                    </Button>
+
+                                                    {this.props.valuesFilters.activeFilterNames.include(button.filterName)
+                                                    ? <CategoryTransactions
+                                                    transactions={dataToDisplay}
+                                                    />
+                                                    : null}
+                                                )
+                                            )
+                                        }
+                                    </div>
+                                )
+                            }
+                        )
+                    }
+
+
+                </div>
             )
-          }
-
-
-        </div>
-      )
-    }
-  })
+        }
+    })
 
