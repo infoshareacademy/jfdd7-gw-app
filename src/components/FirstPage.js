d6520ca0e582b76205532e18886d1a17ae9ca867
TomaszMoroz <tmoroz688@gmail.com>
2017-07-17 07:13:04 +0200

    routing within page with front-page buttons

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import FirstPageButton from './AddComponents/FirstPageButton'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Categories from './Categories'
-import AddTransaction from './AddTransaction'
-import Transactions from './Transactions'
-import Stats from './Stats'
+// import FirstPageButton from './AddComponents/FirstPageButton'
+// import {BrowserRouter as Router, Route} from 'react-router-dom'
+// import Categories from './Categories'
+// import AddTransaction from './AddTransaction'
+// import Transactions from './Transactions'
+// import Stats from './Stats'
 import {Grid, Row, Col, Button} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
 
@@ -16,26 +16,26 @@ export default class FirstPage extends React.Component {
         <div>
           <Grid>
             <Row className="show-grid">
-              <Col xs={6} md={4}>
-                <LinkContainer to="/categories">
-                  <Button bsStyle="primary" bsSize="large" className="custom">Kategorie</Button>
+              <Col xs={6} md={4} lg={3}>
+                <LinkContainer to="/categories" className="custom">
+                  <Button bsStyle="primary" bsSize="large" >Kategorie</Button>
                 </LinkContainer>
               </Col>
-              <Col xs={6} md={4}>
-                <LinkContainer to="/transactions">
-                  <Button bsStyle="primary" bsSize="large" className="custom">Historia</Button>
+              <Col xs={6} md={4} lg={3}>
+                <LinkContainer to="/transactions" className="custom">
+                  <Button bsStyle="primary" bsSize="large" >Historia</Button>
                 </LinkContainer>
               </Col>
-            </Row>
-            <Row className="show-grid">
-              <Col xs={6} md={4}>
-                <LinkContainer to="/add-transaction">
-                  <Button bsStyle="primary" bsSize="large" className="custom">Dodaj wpis</Button>
+            {/*</Row>*/}
+            {/*<Row className="show-grid">*/}
+              <Col xs={6} md={4} lg={3}>
+                <LinkContainer to="/add-transaction" className="custom">
+                  <Button bsStyle="primary" bsSize="large">Dodaj wpis</Button>
                 </LinkContainer>
               </Col>
-              <Col xs={6} md={4}>
-                <LinkContainer to="/stats">
-                  <Button bsStyle="primary" bsSize="large" className="custom">Statystyki</Button>
+              <Col xs={6} md={4} lg={3}>
+                <LinkContainer to="/stats" className="custom">
+                  <Button bsStyle="primary" bsSize="large" >Statystyki</Button>
                 </LinkContainer>
               </Col>
             </Row>
