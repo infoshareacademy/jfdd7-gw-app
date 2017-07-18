13132bfcc572d47f5f8bb53d65daca455b24f9f5
piotrmichalek89 <piotrmichalek89@gmail.com>
2017-07-18 20:47:38 +0200

    new components: Calendar, StatsApplePieView + routing; npm install 'react-big-calendar'

---
+++ src/components/Calendar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Grid} from 'react-bootstrap'
+import {} from 'react-big-calendar'
+
+
+
+export default class Calendar extends React.Component {
+
+  render() {
+
+    return (
+      <Grid>
+        <h1>whatever :) :)</h1>
+
+
+
+
+      </Grid>
+    )
+  }
+}
