053851747ca521c3c8bcadad69a499237a67fb0a
Mateusz Tarsiński <tars.mat@gmail.com>
2017-07-19 16:20:05 +0200

    added icons on the FirstPage buttons

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Grid, Row, Col, Button} from 'react-bootstrap'
+import {Grid, Row, Col, Button, Glyphicon} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
 
 
@@ -12,22 +12,34 @@ export default class FirstPage extends React.Component {
             <Row className="show-grid">
               <Col xs={6} md={4} lg={3}>
                 <LinkContainer to="/categories" className="custom">
-                  <Button bsStyle="warning" bsSize="large" >Kategorie</Button>
+                  <Button bsStyle="warning" bsSize="large" >
+                    Kategorie
+                    <Glyphicon glyph="glyphicon glyphicon-list gi-5x" style={{display: 'block'}} />
+                  </Button>
                 </LinkContainer>
               </Col>
               <Col xs={6} md={4} lg={3}>
                 <LinkContainer to="/transactions" className="custom">
-                  <Button bsStyle="danger" bsSize="large" >Historia</Button>
+                  <Button bsStyle="danger" bsSize="large" >
+                    Historia
+                    <Glyphicon class="icons" glyph="glyphicon glyphicon-folder-open gi-5x" style={{display: 'block'}} />
+                  </Button>
                 </LinkContainer>
               </Col>
               <Col xs={6} md={4} lg={3}>
                 <LinkContainer to="/add-transaction" className="custom">
-                  <Button bsStyle="info" bsSize="large">Dodaj wpis</Button>
+                  <Button bsStyle="info" bsSize="large">
+                    Dodaj wpis
+                    <Glyphicon glyph="glyphicon glyphicon-plus-sign gi-5x" style={{display: 'block'}} />
+                  </Button>
                 </LinkContainer>
               </Col>
               <Col xs={6} md={4} lg={3}>
                 <LinkContainer to="/stats" className="custom">
-                  <Button bsStyle="info" bsSize="large" >Statystyki</Button>
+                  <Button bsStyle="info" bsSize="large" >
+                    Statystyki
+                    <Glyphicon glyph="glyphicon glyphicon-signal gi-5x" style={{display: 'block'}} />
+                  </Button>
                 </LinkContainer>
               </Col>
               <Col xs={12} md={12} lg={12}>
