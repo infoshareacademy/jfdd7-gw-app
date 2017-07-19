b81d399dc5ea8f12383c14cdf78047165cfa81c9
TomaszMoroz <tmoroz688@gmail.com>
2017-07-19 18:44:55 +0200

    login form button added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,8 +4,8 @@
 import React from 'react';
 
 
-import { connect } from 'react-redux';
-import {FormGroup, Col, ControlLabel, FormControl} from 'react-bootstrap'
+
+import {FormGroup, Col, ControlLabel, FormControl, Button} from 'react-bootstrap'
 
 
 export default class LoginForm extends React.Component {
@@ -15,22 +15,29 @@ export default class LoginForm extends React.Component {
 
         return (
             <div>
+                <div className="loginLogo"></div>
                 <FormGroup controlId="formHorizontalEmail">
-                    <Col componentClass={ControlLabel} sm={2}>
-                        Email
-                    </Col>
-                    <Col sm={10}>
+
+                    {/*<Col componentClass={ControlLabel} sm={2}>*/}
+                        {/*Email*/}
+                    {/*</Col>*/}
+                    <Col sm={12}>
                         <FormControl type="email" placeholder="Email" />
                     </Col>
                 </FormGroup>
 
                 <FormGroup controlId="formHorizontalPassword">
-                    <Col componentClass={ControlLabel} sm={2}>
-                        Password
-                    </Col>
-                    <Col sm={10}>
+                    {/*<Col componentClass={ControlLabel} sm={2}>*/}
+                        {/*Password*/}
+                    {/*</Col>*/}
+                    <Col sm={12}>
                         <FormControl type="password" placeholder="Password" />
+                        <Button block
+                                bsStyle="warning"
+                        >Login
+                        </Button>
                     </Col>
+
                 </FormGroup>
             </div>
         )
